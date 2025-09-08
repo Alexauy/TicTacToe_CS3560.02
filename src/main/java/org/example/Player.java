4409b84e3c7cf7ff1587fe413fd4d9706841f68a
Alex <alexauyon2017@gmail.com>
2025-09-07 21:59:56 -0700

    Initial commit: base TicTacToe code

---
+++ src/main/java/org/example/Player.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public abstract class Player {
+    protected final String name;
+    protected final Mark mark;
+
+    protected Player(String name, Mark mark) {
+        this.name = name;
+        this.mark = mark;
+    }
+
+    public String name() { return name; }
+    public Mark mark() { return mark; }
+
+    public abstract int[] chooseMove(Board board);
+}
